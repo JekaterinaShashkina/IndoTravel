8240ab343acb55fd6a555371be38985547251ccd
Catharin <jekaterina.shashkina@gmail.com>
2023-08-01 20:20:39 +0300

    regex[

--- module/modal.js
+++ module/modal.js
@@ -1,81 +1,86 @@
-import {declOfNum} from "./declOfNum.js"
-import loadStyle from "./loadstyle.js";
-import {fetchRequest} from "./sendData.js";
-const URL = 'https://jsonplaceholder.typicode.com/posts'
+import { declOfNum } from './declOfNum.js';
+import loadStyle from './loadstyle.js';
+import { fetchRequest } from './sendData.js';
+const URL = 'https://jsonplaceholder.typicode.com/posts';
 
-export const createModal = async (data)=>{
-await loadStyle('/css/modal.css')
-    const overlay = document.createElement('div')
-    overlay.classList.add('overlay', 'overlay_confirm')
-        const modal = document.createElement('div')
-        modal.classList.add('modal')
-        const title = document.createElement('h2')
-        title.classList.add('modal__title')
-        title.textContent = 'Подтверждение заявки'
-        const peopleCount = document.createElement('p')
-        peopleCount.classList.add('modal__text')
-        const m =declOfNum(data.people.value, ['человек','человека', 'человек'])
-        peopleCount.textContent= `Бронирование путешествия в Индонезию на ${m}`
-        const datesText = document.createElement('p')
-        datesText.classList.add('modal__text')
-        const d = data.dates.value.split(" - ")
-        let month = d[1].slice(3) === '12' ? 'декабря' : 'ноября'
-        datesText.textContent = `В даты: ${d[0].slice(0,2)} ноября - ${d[1].slice(0,2)} ${month}, ${m}`
-        const priceText = document.createElement('p')
-        priceText.classList.add('modal__text')
-        const price=document.querySelector('.reservation__price')
-        priceText.textContent = `Стоимость тура ${price.textContent}`
+export const createModal = async (data) => {
+  await loadStyle('/css/modal.css');
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay', 'overlay_confirm');
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  const title = document.createElement('h2');
+  title.classList.add('modal__title');
+  title.textContent = 'Подтверждение заявки';
+  const peopleCount = document.createElement('p');
+  peopleCount.classList.add('modal__text');
+  const m = declOfNum(data.people.value, ['человек', 'человека', 'человек']);
+  peopleCount.textContent = `Бронирование путешествия в Индонезию на ${m}`;
+  const datesText = document.createElement('p');
+  datesText.classList.add('modal__text');
+  const d = data.dates.value.split(' - ');
+  let month = d[1].slice(3) === '12' ? 'декабря' : 'ноября';
+  datesText.textContent = `В даты: ${d[0].slice(0, 2)} ноября - ${d[1].slice(
+    0,
+    2,
+  )} ${month}, ${m}`;
+  const priceText = document.createElement('p');
+  priceText.classList.add('modal__text');
+  const price = document.querySelector('.reservation__price');
+  priceText.textContent = `Стоимость тура ${price.textContent}`;
 
-        const buttons =document.createElement('div')
-        buttons.classList.add('modal__button')
-        const btnConfirm = document.createElement('button')
-        btnConfirm.classList.add('modal__btn', 'modal__btn_confirm')
-        btnConfirm.textContent= 'Подтверждаю'
-        const btnEdit = document.createElement('button')
-        btnEdit.classList.add('modal__btn', 'modal__btn_edit')
-        btnEdit.textContent = 'Изменить данные'
-        buttons.append(btnConfirm, btnEdit)
-        modal.append(title, peopleCount, datesText,priceText, buttons)
-        overlay.append(modal)
-        document.body.append(overlay)
-        btnEdit.addEventListener('click', () => {
-            overlay.remove()
-        })
-        btnConfirm.addEventListener('click',(e) =>{
-            e.preventDefault()
-                fetchRequest(URL, {
-                    method: 'POST',
-                    body: {
-                        dates: data.dates.value,
-                        people: data.people.value,
-                        name: data.name.value,
-                        phone: data.phone.value
-                    },
-                    callback: (err, data)=>{
-                        if (err) {
-                            console.warn(err, data)
-                          return
-                        }
-                        overlay.remove()
-                        const selects = document.querySelectorAll('.reservation__select')
-                        selects.forEach(elem => {
-                            elem.value = ''
-                            elem.setAttribute('disabled', 'true')
-                        })
-                        const inputs =document.querySelectorAll('.reservation__input')
-                        inputs.forEach(elem => {
-                            elem.value = ''
-                            elem.setAttribute('disabled', 'true')
-                        })
-                        document.querySelector('.reservation__button').setAttribute('disabled', true)
-                        document.querySelector('.reservation__data').textContent= 'Ваша заявка принята'
-                        document.querySelector('.reservation__price').textContent= ''
+  const buttons = document.createElement('div');
+  buttons.classList.add('modal__button');
+  const btnConfirm = document.createElement('button');
+  btnConfirm.classList.add('modal__btn', 'modal__btn_confirm');
+  btnConfirm.textContent = 'Подтверждаю';
+  const btnEdit = document.createElement('button');
+  btnEdit.classList.add('modal__btn', 'modal__btn_edit');
+  btnEdit.textContent = 'Изменить данные';
+  buttons.append(btnConfirm, btnEdit);
+  modal.append(title, peopleCount, datesText, priceText, buttons);
+  overlay.append(modal);
+  document.body.append(overlay);
+  btnEdit.addEventListener('click', () => {
+    overlay.remove();
+  });
 
-                    },
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }) 
-        })
-    }
-
+  btnConfirm.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetchRequest(URL, {
+      method: 'POST',
+      body: {
+        dates: data.dates.value,
+        people: data.people.value,
+        name: data.name.value,
+        phone: data.phone.value,
+      },
+      callback: (err, data) => {
+        if (err) {
+          console.warn(err, data);
+          return;
+        }
+        overlay.remove();
+        const selects = document.querySelectorAll('.reservation__select');
+        selects.forEach((elem) => {
+          elem.value = '';
+          elem.setAttribute('disabled', 'true');
+        });
+        const inputs = document.querySelectorAll('.reservation__input');
+        inputs.forEach((elem) => {
+          elem.value = '';
+          elem.setAttribute('disabled', 'true');
+        });
+        document
+          .querySelector('.reservation__button')
+          .setAttribute('disabled', true);
+        document.querySelector('.reservation__data').textContent =
+          'Ваша заявка принята';
+        document.querySelector('.reservation__price').textContent = '';
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+};
--- module/renderDates.js
+++ module/renderDates.js
@@ -1,10 +1,13 @@
-import {declOfNum} from "./declOfNum.js";
-import {createModal} from "./modal.js";
+import { declOfNum } from './declOfNum.js';
+import { createModal } from './modal.js';
+const form = document.querySelector('.reservation__form');
+const reservDate = document.querySelector('.reservation__data');
+const price = document.querySelector('.reservation__price');
 
 export const renderDates = async (err, data) => {
   if (err) {
-    console.warn(err, data)
-    return
+    console.warn(err, data);
+    return;
   }
   const selects = document.querySelectorAll('.tour__select');
   const dates = data.map((item) => {
@@ -19,26 +22,26 @@ export const renderDates = async (err, data) => {
     selects[1].innerHTML = `                  
       <option value="" class="tour__option">
         Количество человек
-      </option>`
-      data.forEach((element) => {
-        if (element.date === e.target.value) {
-          for (let i = element.min_people; i <= element.max_people; i++) {
-            const peopleOption = document.createElement('option')
-            peopleOption.classList.add('tour__option')
-            peopleOption.value =i
-            peopleOption.textContent = i
-           selects[1].append(peopleOption)
-          }
+      </option>`;
+    data.forEach((element) => {
+      if (element.date === e.target.value) {
+        for (let i = element.min_people; i <= element.max_people; i++) {
+          const peopleOption = document.createElement('option');
+          peopleOption.classList.add('tour__option');
+          peopleOption.value = i;
+          peopleOption.textContent = i;
+          selects[1].append(peopleOption);
         }
-      });
-  })
+      }
+    });
+  });
 };
 
 export const renderReserve = async (err, data) => {
-    if (err) {
-      console.warn(err, data)
-      return
-    }
+  if (err) {
+    console.warn(err, data);
+    return;
+  }
   const selects = document.querySelectorAll('.reservation__select');
   const dates = data.map((item) => {
     const option = document.createElement('option');
@@ -52,46 +55,59 @@ export const renderReserve = async (err, data) => {
     selects[1].innerHTML = `                  
       <option value="" class="tour__option">
         Количество человек
-      </option>`
-      data.forEach((element) => {
-        if (element.date === e.target.value) {
-          for (let i = element.min_people; i <= element.max_people; i++) {
-            const peopleOption = document.createElement('option')
-            peopleOption.classList.add('tour__option', 'reservation__option')
-            peopleOption.value =i
-            peopleOption.textContent = i
-           selects[1].append(peopleOption)
-          }
+      </option>`;
+    data.forEach((element) => {
+      if (element.date === e.target.value) {
+        for (let i = element.min_people; i <= element.max_people; i++) {
+          const peopleOption = document.createElement('option');
+          peopleOption.classList.add('tour__option', 'reservation__option');
+          peopleOption.value = i;
+          peopleOption.textContent = i;
+          selects[1].append(peopleOption);
         }
-      });
-      
-  })
-  const peopleReserv = document.querySelector('#reservation__people')
-  const price = document.querySelector('.reservation__price')
-    price.textContent = ''
-  const reservDate = document.querySelector('.reservation__data')
-    reservDate.textContent=''
-  peopleReserv.addEventListener('change', (e) =>{
-    const date =document.querySelector('#reservation__date')
-    const m = declOfNum(e.target.value, ['человек','человека', 'человек'])
-    const d = date.value.split(" - ")
-    let month = d[1].slice(3) === '12' ? 'декабря' : 'ноября'
-    reservDate.textContent = `${d[0].slice(0,2)} ноября - ${d[1].slice(0,2)} ${month}, ${m}`
-    data.forEach(elem => {
-      if(elem.date === date.value) {
-        const allprice = e.target.value * elem.price
-        price.textContent = `${allprice}₽`
       }
-    })
-  })
-  const form = document.querySelector('.reservation__form')
-  const btn = document.querySelector('.reservation__button')
+    });
+  });
+  const peopleReserv = document.querySelector('#reservation__people');
+  price.textContent = '';
+  reservDate.textContent = '';
+  peopleReserv.addEventListener('change', (e) => {
+    const date = document.querySelector('#reservation__date');
+    const m = declOfNum(e.target.value, ['человек', 'человека', 'человек']);
+    const d = date.value.split(' - ');
+    let month = d[1].slice(3) === '12' ? 'декабря' : 'ноября';
+    reservDate.textContent = `${d[0].slice(0, 2)} ноября - ${d[1].slice(
+      0,
+      2,
+    )} ${month}, ${m}`;
+    data.forEach((elem) => {
+      if (elem.date === date.value) {
+        const allprice = e.target.value * elem.price;
+        price.textContent = `${allprice}₽`;
+      }
+    });
+  });
+  form.addEventListener('input', () => {
+    form.name.value = form.name.value.replace(/[^а-яА-ЯёЁ\s]+$/i, '');
+  });
+
+  form.addEventListener('input', () => {
+    form.phone.value = form.phone.value.replace(/[^+\0-9\s]+$/i, '');
+  });
+  const reg = /^([^\s]*\s){2,}[^\s]*$/i;
+
+  const btn = document.querySelector('.reservation__button');
   form.addEventListener('click', (e) => {
-    e.preventDefault()
-    if(e.target === btn) {
-      createModal(form)
+    e.preventDefault();
+    if (e.target === btn) {
+      if (reg.test(form.name.value)) {
+        createModal(form);
+      } else {
+        const info = document.querySelector('.reservation__info');
+        const nameErr = document.createElement('p');
+        nameErr.textContent = 'Неправильно введено ФИО';
+        info.append(nameErr);
+      }
     }
-  })
-}
-
-
+  });
+};
